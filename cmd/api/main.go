9deb2a309e0782c073cdbb2597f2734bee7fcb57
Nurtilek <nurtilek.develop@gmail.com>
2023-10-17 17:32:42 +0600

    initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"runtime/debug"
+	"sync"
+
+	"github.com/nurtidev/govtest/internal/database"
+	"github.com/nurtidev/govtest/internal/env"
+	"github.com/nurtidev/govtest/internal/version"
+
+	"github.com/lmittmann/tint"
+)
+
+func main() {
+	logger := slog.New(tint.NewHandler(os.Stdout, &tint.Options{Level: slog.LevelDebug}))
+
+	err := run(logger)
+	if err != nil {
+		trace := string(debug.Stack())
+		logger.Error(err.Error(), "trace", trace)
+		os.Exit(1)
+	}
+}
+
+type config struct {
+	baseURL   string
+	httpPort  int
+	basicAuth struct {
+		username       string
+		hashedPassword string
+	}
+	db struct {
+		dsn         string
+		automigrate bool
+	}
+	jwt struct {
+		secretKey string
+	}
+}
+
+type application struct {
+	config config
+	db     *database.DB
+	logger *slog.Logger
+	wg     sync.WaitGroup
+}
+
+func run(logger *slog.Logger) error {
+	var cfg config
+
+	cfg.baseURL = env.GetString("BASE_URL", "http://localhost:4444")
+	cfg.httpPort = env.GetInt("HTTP_PORT", 4444)
+	cfg.basicAuth.username = env.GetString("BASIC_AUTH_USERNAME", "admin")
+	cfg.basicAuth.hashedPassword = env.GetString("BASIC_AUTH_HASHED_PASSWORD", "$2a$10$jRb2qniNcoCyQM23T59RfeEQUbgdAXfR6S0scynmKfJa5Gj3arGJa")
+	cfg.db.dsn = env.GetString("DB_DSN", "admin:zxc123!@localhost:5432/govtest?sslmode=disable")
+	cfg.db.automigrate = env.GetBool("DB_AUTOMIGRATE", true)
+	cfg.jwt.secretKey = env.GetString("JWT_SECRET_KEY", "xclnfnvtskgtvdc54hagwzgmubybgc5u")
+
+	showVersion := flag.Bool("version", false, "display version and exit")
+
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("version: %s\n", version.Get())
+		return nil
+	}
+
+	db, err := database.New(cfg.db.dsn, cfg.db.automigrate)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	app := &application{
+		config: cfg,
+		db:     db,
+		logger: logger,
+	}
+
+	return app.serveHTTP()
+}
---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/nurtidev/govtest
+
+go 1.21.0
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/golang-migrate/migrate/v4 v4.16.2
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.9
+	github.com/lmittmann/tint v1.0.2
+	github.com/pascaldekloe/jwt v1.12.0
+	golang.org/x/crypto v0.14.0
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
